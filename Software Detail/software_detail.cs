ccfd4388cd23e181972bc0a14ed294ce0c3222c3
pawangupta097 <99266073+pawangupta097@users.noreply.github.com>
2023-03-03 20:32:25 +0530

    Add files via upload

---
+++ Software Detail/software_detail.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace Software_Data
+{
+    class software_detail
+    {
+        public static void Main()
+        {
+            Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+            Console.WriteLine("{0, -80} {1,-10}", "SOFTWARE", "VERSION");
+            Console.WriteLine("{0, -80} {1,-10}", "========", "=======");
+            foreach (var v in key.GetSubKeyNames())
+            {
+                string keyName = key + "\\" + v;
+                string a = (string)Registry.GetValue(keyName, "DisplayName", null);
+
+                if (a != null)
+                {
+                    try
+                    {
+                        Console.WriteLine("{0, -80} {1,-10}", Registry.GetValue(keyName, "DisplayName", null), Registry.GetValue(keyName, "DisplayVersion", null));
+                    }
+                    catch (Exception e)
+                    { }
+                }
+            }
+
+
+        }
+    }
+}
